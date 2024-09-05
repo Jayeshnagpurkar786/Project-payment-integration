365169bf178ff082e630685ae09f3d8874b2a135
Jayesh Nagpurkar <jayeshnagpurkar2@gmail.com>
2024-09-05 11:45:06 +0530

    sixth commit

--- backend/models/database.js
+++ backend/models/database.js
@@ -1,25 +1,32 @@
-const { Pool } = require('pg');
-const dotenv = require('dotenv');
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
 
 // Load environment variables from .env file
 dotenv.config();
 
 // Create a new pool instance
 const pool = new Pool({
-  user: process.env.DATABASE_USER,
-  host: process.env.DATABASE_HOST,
-  database: process.env.DATABASE_NAME,
-  password: process.env.DATABASE_PASSWORD,
-  port: process.env.DATABASE_PORT || 5432 ,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
-// Connect to the database
-pool.connect()
-  .then(() => console.log('Connected to the database'))
-  .catch((err) => {
-    console.error('Connection error', err.stack);
-    process.exit(1); // Exit the process with an error code
-  });
 
-// Export the pool for use in other modules
-module.exports = { pool };
+export default async function handler(req, res) {
+  try {
+    // Connect to the database
+    const client = await pool.connect();
+    console.log("Connected to database");
+
+    // Perform a query
+    const result = await client.query('SELECT * FROM rzp_payments');
+    console.log("Query result:", result);
+
+    // Send the query result as a JSON response
+    res.status(200).json(result.rows);
+
+    // Release the client back to the pool
+    client.release();
+  } catch (err) {
+    console.error('Database connection error:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
